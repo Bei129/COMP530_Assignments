843d7f5bd28066a922150bfb9244e39f78e38616
plumsorbet <wutt0509@gmail.com>
2024-10-31 00:05:10 -0500

    Implement RegularSelection and BPlusSelection

--- A5/Main/RelOps/source/BPlusSelection.cc
+++ A5/Main/RelOps/source/BPlusSelection.cc
@@ -4,10 +4,46 @@
 
 #include "BPlusSelection.h"
 
-BPlusSelection :: BPlusSelection (MyDB_BPlusTreeReaderWriterPtr, MyDB_TableReaderWriterPtr,
-                MyDB_AttValPtr, MyDB_AttValPtr,
-                string, vector <string>) {}
+BPlusSelection :: BPlusSelection (MyDB_BPlusTreeReaderWriterPtr input, MyDB_TableReaderWriterPtr output,
+                MyDB_AttValPtr low, MyDB_AttValPtr high,
+                string selectionPredicate, vector <string> projections) {
+    this->input = input;
+    this->output = output;
+    this->low = low;
+    this->high = high;
+    this->selectionPredicate = selectionPredicate;
+    this->projections = projections;
+}
 
-void BPlusSelection :: run () {}
+void BPlusSelection :: run () {
+    MyDB_RecordPtr inputRec = input->getEmptyRecord();
+
+    func predicate = inputRec->compileComputation(selectionPredicate);
+
+    // and get the final set of computatoins that will be used to buld the output record
+    vector <func> finalComputations;
+    for (string s : projections) {
+        finalComputations.push_back(inputRec->compileComputation(s));
+    }
+
+    MyDB_RecordPtr outputRec = output->getEmptyRecord();
+
+    MyDB_RecordIteratorAltPtr iter = input->getRangeIteratorAlt(low, high);
+    while (iter->advance()) {
+        iter->getCurrent(inputRec);
+
+        if (predicate()->toBool()) {
+
+            // run all of the computations
+            int i = 0;
+            for (auto& f : finalComputations) {
+                outputRec->getAtt(i++)->set(f());
+            }
+
+            outputRec->recordContentHasChanged();
+            output->append(outputRec);
+        }
+    }
+}
 
 #endif
--- A5/Main/RelOps/source/RegularSelection.cc
+++ A5/Main/RelOps/source/RegularSelection.cc
@@ -4,9 +4,43 @@
 
 #include "RegularSelection.h"
 
-RegularSelection :: RegularSelection (MyDB_TableReaderWriterPtr, MyDB_TableReaderWriterPtr,
-                string, vector <string>) {}
+RegularSelection :: RegularSelection (MyDB_TableReaderWriterPtr input, MyDB_TableReaderWriterPtr output,
+                string selectionPredicate, vector <string> projections) {
+    this->input = input;
+    this->output = output;
+    this->selectionPredicate = selectionPredicate;
+    this->projections = projections;
+}
 
-void RegularSelection :: run () {}
+void RegularSelection :: run () {
+    MyDB_RecordPtr inputRec = input->getEmptyRecord();
+
+    func predicate = inputRec->compileComputation(selectionPredicate);
+
+    // and get the final set of computatoins that will be used to buld the output record
+    vector <func> finalComputations;
+    for (string s : projections) {
+        finalComputations.push_back(inputRec->compileComputation(s));
+    }
+
+    MyDB_RecordPtr outputRec = output->getEmptyRecord();
+
+    MyDB_RecordIteratorAltPtr iter = input->getIteratorAlt();
+    while (iter->advance()) {
+        iter->getCurrent(inputRec);
+
+        if (predicate()->toBool()) {
+
+            // run all of the computations
+            int i = 0;
+            for (auto& f : finalComputations) {
+                outputRec->getAtt(i++)->set(f());
+            }
+
+            outputRec->recordContentHasChanged();
+            output->append(outputRec);
+        }
+    }
+}
 
 #endif
