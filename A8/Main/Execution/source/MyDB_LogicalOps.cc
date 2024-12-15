d24662020ee696d8a69562fd5e53d8e57804fbcc
plumsorbet <wutt0509@gmail.com>
2024-12-14 21:26:10 -0600

    Draft LogicalJoin :: execute

--- A8/Main/Execution/source/MyDB_LogicalOps.cc
+++ A8/Main/Execution/source/MyDB_LogicalOps.cc
@@ -8,6 +8,7 @@
 #include "RegularSelection.h"
 #include "ScanJoin.h"
 #include "SortMergeJoin.h"
+#include <algorithm>
 
 string generateSelectionPredicate(vector<ExprTreePtr> exprs)
 {
@@ -25,17 +26,33 @@ string generateSelectionPredicate(vector<ExprTreePtr> exprs)
     return result;
 }
 
+string generateSelectionPredicateJoin(vector<ExprTreePtr> exprs)
+{
+    string result = "";
+    if (exprs.size() == 0) {
+        return "bool[true]";
+    }
+
+    result = exprs[0]->toString();
+
+    for (int i = 1; i < exprs.size(); i++) {
+        result = "&& (" + result + ", " + exprs[i]->toString() + ")";
+    }
+
+    return result;
+}
+
 MyDB_TableReaderWriterPtr LogicalTableScan :: execute (map <string, MyDB_TableReaderWriterPtr> &allTableReaderWriters,
 	map <string, MyDB_BPlusTreeReaderWriterPtr> &allBPlusReaderWriters) {
 	
 	// your code here!
-	cout << "DEBUG: Scan start!" << endl;
+	/*cout << "DEBUG: Scan start!" << endl;
 	cout << "input schema: " << inputSpec->getSchema() << endl;
 	cout << "output schema: " << outputSpec->getSchema() << endl;
 	cout << "selection predicate: " << endl;
 	for (auto x : selectionPred) {
 		cout <<"  "<< x->toString() << endl;
-	}
+	}*/
 
     string inputTableName = this->getInputTableName();
     MyDB_TableReaderWriterPtr inputTable = allTableReaderWriters[inputTableName];
@@ -43,18 +60,18 @@ MyDB_TableReaderWriterPtr LogicalTableScan :: execute (map <string, MyDB_TableRe
     
     vector<string> projections;
 
-    cout << "DEBUG: projections:" << endl;
+    //cout << "DEBUG: projections:" << endl;
     for (auto att : outputSpec->getSchema()->getAtts()) {
         string attName = att.first;
         size_t pos = attName.find('_');
         attName = attName.substr(pos + 1);
         projections.push_back("["+ attName +"]");
-        cout << "[" + attName + "]" << endl;
+        //cout << "[" + attName + "]" << endl;
     }
 
     string selectionPredStr = generateSelectionPredicate(selectionPred);
-    cout << "DEBUG: selectionPredicate: " << endl;
-    cout << "  " << selectionPredStr << endl << endl;
+    //cout << "DEBUG: selectionPredicate: " << endl;
+    //cout << "  " << selectionPredStr << endl << endl;
 
     RegularSelection regularSelection(inputTable, outputTable, selectionPredStr, projections);
     regularSelection.run();
@@ -66,8 +83,76 @@ MyDB_TableReaderWriterPtr LogicalJoin :: execute (map <string, MyDB_TableReaderW
 	map <string, MyDB_BPlusTreeReaderWriterPtr> &allBPlusReaderWriters) {
 
 	// your code here!
+    /*cout << "DEBUG: Join start!" << endl;
+    cout << "leftInputOp print: " << endl;
+    leftInputOp->print();
+    cout << "rightInputOp print: " << endl;
+    rightInputOp->print();
+    cout << "output schema: " << outputSpec->getSchema() << endl;
+    cout << "selection predicate: " << endl;
+    for (auto x : outputSelectionPredicate) {
+        cout << "  " << x->toString() << endl;
+    }*/
 
-	return nullptr;
+    MyDB_TableReaderWriterPtr leftTable = leftInputOp->execute(allTableReaderWriters, allBPlusReaderWriters);
+    MyDB_TableReaderWriterPtr rightTable = rightInputOp->execute(allTableReaderWriters, allBPlusReaderWriters);
+
+    MyDB_TableReaderWriterPtr outputTable = make_shared<MyDB_TableReaderWriter>(outputSpec, leftTable->getBufferMgr());
+
+    vector<string> projections;
+
+    //cout << "DEBUG: projections:" << endl;
+    for (auto att : outputSpec->getSchema()->getAtts()) {
+        string attName = att.first;
+        projections.push_back("[" + attName + "]");
+        //cout << "[" + attName + "]" << endl;
+    }
+
+    string selectionPredStr = generateSelectionPredicateJoin(outputSelectionPredicate);
+    vector <pair <string, MyDB_AttTypePtr>> leftAtts = leftTable->getTable()->getSchema()->getAtts();
+    vector <pair <string, MyDB_AttTypePtr>> rightAtts = rightTable->getTable()->getSchema()->getAtts();
+    cout << "DEBUG: leftAtts:" << endl;
+    for (const auto& att : leftAtts) {
+        string attName = att.first;
+        string attType = att.second->toString();
+        cout << "  Attribute Name: " << attName << ", Attribute Type: " << attType << endl;
+    }
+
+    cout << "DEBUG: rightAtts:" << endl;
+    for (const auto& att : rightAtts) {
+        string attName = att.first;
+        string attType = att.second->toString();
+        cout << "  Attribute Name: " << attName << ", Attribute Type: " << attType << endl;
+    }
+
+
+    pair <string, string> equalityCheck;
+    for (auto expr : outputSelectionPredicate) {
+        if (expr->getLHS() && expr->getRHS())
+        {
+            string lhs = expr->getLHS()->toString();
+            string rhs = expr->getRHS()->toString();
+            if (any_of(leftAtts.begin(), leftAtts.end(), [&lhs](const pair<string, MyDB_AttTypePtr>& p)
+                { return lhs == "[" + p.first + "]"; }))
+                equalityCheck = make_pair(lhs, rhs);
+            else
+                equalityCheck = make_pair(rhs, lhs);
+            break;
+        }
+    }
+    cout << "equalityCheck: " << equalityCheck.first <<", " << equalityCheck.second << endl;
+
+    string leftSelectionPredicate = "bool[true]";
+    string rightSelectionPredicate = "bool[true]";
+
+    SortMergeJoin sortMergeJoin(leftTable, rightTable, outputTable, selectionPredStr, projections, equalityCheck, leftSelectionPredicate, rightSelectionPredicate);
+    sortMergeJoin.run();
+
+    MyDB_BufferManagerPtr bufferMgr = leftTable->getBufferMgr();
+    bufferMgr->killTable(leftTable->getTable());
+    bufferMgr->killTable(rightTable->getTable());
+
+	return outputTable;
 }
 
 #endif
